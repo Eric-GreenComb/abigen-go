0d0d1ba8608ebecbcda2de3ddd4719e66dc050bf
eric <ministor@126.com>
2018-04-12 19:00:27 +0800

    test parity

--- token/README.md
+++ token/README.md
@@ -1,2 +1,31 @@
+# Token
+
+## abigen
 
 abigen --sol HumanStandardToken.sol -pkg main --lang go --out cc.go
+
+
+solc --libraries BigInt:0x6fee4929bbe6156903ffa602f2eb997ea6c8b20f --bin --abi --overwrite Test.sol -o ./
+abigen --abi Test.abi --bin Test.bin -solc /usr/local/bin/solc -type Test -pkg main --lang go --out Test.go
+
+## run
+
+- deploy
+
+curl -s -X POST http://localhost:3000/deploy 
+  
+- send
+
+curl -s -X POST http://localhost:3000/send/:conaddr/:to/:amount
+
+curl -s -X POST http://localhost:3000/send/7ba4324585cb5597adc283024819254345cd7c62/00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2/10000
+
+curl -s -X POST http://localhost:3000/send/7ba4324585cb5597adc283024819254345cd7c62/0x008f0194Bf7B1dA7528132Ed098D4351168eB77b/100000
+
+curl -s -X POST http://localhost:3000/ericsend/7ba4324585cb5597adc283024819254345cd7c62/0x00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2/10000
+
+- get
+
+http://localhost:3000/get/7ba4324585cb5597adc283024819254345cd7c62/00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2
+
+granola robin cone reverb stick pristine entryway flint canopy landing walnut finite
--- token/main.go
+++ token/main.go
@@ -14,11 +14,13 @@ import (
 )
 
 // config address info
-const key = `{"address":"73e091f128342f54595f6bde83970cb5ec84a6bf","crypto":{"cipher":"aes-128-ctr","ciphertext":"005ba9999e7be29352eb40b95734596c4ecf4ad26ea65b0d1ca28521fa7a2d80","cipherparams":{"iv":"c2203f266a4f442508733a30f001274b"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"600353ee2885f0238d912734a0638869b4ff73f50d3a839a1dfd800bed7051de"},"mac":"c4dcc6de182c9a30aa233883785b06a150000349340f504ee827068ba791fa60"},"id":"753f08b0-04f9-4b9b-8391-070583f6d13d","version":3}`
-const pwd = `a1111111`
+const key = `{"address":"004ec07d2329997267ec62b4166639513386f32e","crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"87b493d4dd7f328e4c3d4400c306b552"},"ciphertext":"4853877ee45b1ec41dbedc2d237260aef2291d5dca6f640e053e40cb0686343d","kdf":"pbkdf2","kdfparams":{"c":10240,"dklen":32,"prf":"hmac-sha256","salt":"e992921213cb010c901f368b761b91cfab956ec6a1371617af985b1fe5af791c"},"mac":"43c63c503b872f0a9312ac5c5c24a0039edcb708c54e289e6db096341b80c851"},"id":"ca5df0e3-c4c5-9b56-0457-9e23e47b3221","meta":"{}","name":"","version":3}`
+const pwd = `user`
+const eric = `{"address":"008f0194bf7b1da7528132ed098d4351168eb77b","crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"533a6eb7934a1c4649ee56e613d66bff"},"ciphertext":"062dbca93704bb1ec3018a78752f7b4870e0cdf7a93b04392547152fd162c74d","kdf":"pbkdf2","kdfparams":{"c":10240,"dklen":32,"prf":"hmac-sha256","salt":"56f335f8a29d87feb97ff2931258b58598f475f53346e26c9d6c9df0f0c6afef"},"mac":"eeabffc95026e0b8a01f686ff7136a24d704719231baaf2efdcd6e7d5c3fe67e"},"id":"4aab680f-ad79-7fca-ef4e-7ab3f46d1919","meta":"{\"description\":\"\",\"passwordHint\":\"\",\"timestamp\":1523529995560}","name":"eric","version":3}`
+const ericpwd = `a1111111`
 
 func main() {
-	client, err := ethclient.Dial("/home/eric/.ethereum/geth.ipc")
+	client, err := ethclient.Dial("http://localhost:8540")
 	if err != nil {
 		fmt.Println("Failed to connect to the Ethereum client:", err)
 		os.Exit(0)
@@ -67,6 +69,35 @@ func main() {
 		}
 
 		txOpt, err := bind.NewTransactor(strings.NewReader(key), pwd)
+		if err != nil {
+			c.String(200, err.Error())
+			return
+		}
+		ts, _ := NewHumanStandardTokenTransactor(common.HexToAddress(_conaddr), client)
+
+		_bigint := big.NewInt(_int64)
+		_, err = ts.Transfer(txOpt, common.HexToAddress(_to), _bigint)
+		if err != nil {
+			c.String(200, err.Error())
+			return
+		}
+
+		c.String(200, "OK")
+	})
+
+	// step 2. SendCoin
+	r.POST("/ericsend/:conaddr/:to/:amount", func(c *gin.Context) {
+
+		_conaddr := c.Params.ByName("conaddr")
+		_to := c.Params.ByName("to")
+		_amount := c.Params.ByName("amount")
+		_int64, err := strconv.ParseInt(_amount, 10, 64)
+		if err != nil {
+			c.String(200, err.Error())
+			return
+		}
+
+		txOpt, err := bind.NewTransactor(strings.NewReader(eric), ericpwd)
 		if err != nil {
 			c.String(200, err.Error())
 			return
