e4e84a6c4482ae5141a49a29ca251cf0af7fa4d7
eric <ministor@126.com>
2018-04-13 18:19:14 +0800

    add eric 2 alan

--- token/README.md
+++ token/README.md
@@ -18,14 +18,26 @@ curl -s -X POST http://localhost:3000/deploy
 
 curl -s -X POST http://localhost:3000/send/:conaddr/:to/:amount
 
-curl -s -X POST http://localhost:3000/send/7ba4324585cb5597adc283024819254345cd7c62/00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2/10000
+curl -s -X POST http://localhost:3000/send/0xEa4C690E7560070071643960f20bA984c8A922cd/0x00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2/1025
 
-curl -s -X POST http://localhost:3000/send/7ba4324585cb5597adc283024819254345cd7c62/0x008f0194Bf7B1dA7528132Ed098D4351168eB77b/100000
+- send 2 eric
 
-curl -s -X POST http://localhost:3000/ericsend/7ba4324585cb5597adc283024819254345cd7c62/0x00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2/10000
+curl -s -X POST http://localhost:3000/send/0xEa4C690E7560070071643960f20bA984c8A922cd/0x00eaf5c65f32917c922364e4892b5ec5a35baa6e/1000000
+
+- eric send 2 alan
+
+curl -s -X POST http://localhost:3000/eric/send/0xEa4C690E7560070071643960f20bA984c8A922cd/0x00a5524ceac2e06c7f48085fb07d0913cfcda2b6/10000
 
 - get
 
-http://localhost:3000/get/7ba4324585cb5597adc283024819254345cd7c62/00Aa39d30F0D20FF03a22cCfc30B7EfbFca597C2
+http://localhost:3000/get/0xEa4C690E7560070071643960f20bA984c8A922cd/0x00eaf5c65f32917c922364e4892b5ec5a35baa6e
 
-granola robin cone reverb stick pristine entryway flint canopy landing walnut finite
+- add user
+
+curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["eric", "eric"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8541
+
+0x00eaf5c65f32917c922364e4892b5ec5a35baa6e
+
+curl --data '{"jsonrpc":"2.0","method":"parity_newAccountFromPhrase","params":["alan", "alan"],"id":0}' -H "Content-Type: application/json" -X POST localhost:8541
+
+0x00a5524ceac2e06c7f48085fb07d0913cfcda2b6
--- token/main.go
+++ token/main.go
@@ -16,8 +16,8 @@ import (
 // config address info
 const key = `{"address":"004ec07d2329997267ec62b4166639513386f32e","crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"87b493d4dd7f328e4c3d4400c306b552"},"ciphertext":"4853877ee45b1ec41dbedc2d237260aef2291d5dca6f640e053e40cb0686343d","kdf":"pbkdf2","kdfparams":{"c":10240,"dklen":32,"prf":"hmac-sha256","salt":"e992921213cb010c901f368b761b91cfab956ec6a1371617af985b1fe5af791c"},"mac":"43c63c503b872f0a9312ac5c5c24a0039edcb708c54e289e6db096341b80c851"},"id":"ca5df0e3-c4c5-9b56-0457-9e23e47b3221","meta":"{}","name":"","version":3}`
 const pwd = `user`
-const eric = `{"address":"008f0194bf7b1da7528132ed098d4351168eb77b","crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"533a6eb7934a1c4649ee56e613d66bff"},"ciphertext":"062dbca93704bb1ec3018a78752f7b4870e0cdf7a93b04392547152fd162c74d","kdf":"pbkdf2","kdfparams":{"c":10240,"dklen":32,"prf":"hmac-sha256","salt":"56f335f8a29d87feb97ff2931258b58598f475f53346e26c9d6c9df0f0c6afef"},"mac":"eeabffc95026e0b8a01f686ff7136a24d704719231baaf2efdcd6e7d5c3fe67e"},"id":"4aab680f-ad79-7fca-ef4e-7ab3f46d1919","meta":"{\"description\":\"\",\"passwordHint\":\"\",\"timestamp\":1523529995560}","name":"eric","version":3}`
-const ericpwd = `a1111111`
+const eric = `{"address":"00eaf5c65f32917c922364e4892b5ec5a35baa6e","crypto":{"cipher":"aes-128-ctr","cipherparams":{"iv":"7d2ea255e51c5a20530744590bb51bba"},"ciphertext":"09b5f7ca2fe47a9f522da8a87207e2ada9c93858c915c891c1e5663695bad669","kdf":"pbkdf2","kdfparams":{"c":10240,"dklen":32,"prf":"hmac-sha256","salt":"e2ad7727dae631c8dc00f1efc656bede4945532e2a6736412363d2f8e9838336"},"mac":"4096a009a108fd0a6462d373eb68e0e4bfd885d2061b33029c1d8296754b5015"},"id":"8cca6ede-dc66-b2a8-dfe9-435803d2a474","meta":"{}","name":"","version":3}`
+const ericpwd = `eric`
 
 func main() {
 	client, err := ethclient.Dial("http://localhost:8540")
@@ -86,7 +86,7 @@ func main() {
 	})
 
 	// step 2. SendCoin
-	r.POST("/ericsend/:conaddr/:to/:amount", func(c *gin.Context) {
+	r.POST("/eric/send/:conaddr/:to/:amount", func(c *gin.Context) {
 
 		_conaddr := c.Params.ByName("conaddr")
 		_to := c.Params.ByName("to")
